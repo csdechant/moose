0cf24375f5e959ad766a86ef25ac28123af13936
csdechant <csdechan@ncsu.edu>
2024-11-19 12:38:20 -0700

    Editing FE-to-FV coupling based on current MOOSE version

--- framework/src/problems/SubProblem.C
+++ framework/src/problems/SubProblem.C
@@ -1282,10 +1282,12 @@ SubProblem::getCoordSystem(SubdomainID sid) const
 }
 
 void
-SubProblem::reinitFVFace(const THREAD_ID tid, const FaceInfo & fi)
+// SubProblem::reinitFVFace(const THREAD_ID tid, const FaceInfo & fi)
+SubProblem::reinitFVFace(const THREAD_ID tid, const FaceInfo & fi, bool areFE)
 {
   for (const auto nl : make_range(numNonlinearSystems()))
-    assembly(tid, nl).reinitFVFace(fi);
+    assembly(tid, nl).reinitFVFace(fi, areFE);
+    // assembly(tid, nl).reinitFVFace(fi);
 }
 
 void
